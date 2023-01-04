aa11126adf6345f8e573c9d40d008680d8cb2b44
John Novak <janovak@mtu.edu>
2023-01-03 22:25:47 -0800

    Clean up the python

--- process_image.py
+++ process_image.py
@@ -11,6 +11,78 @@ from typing import Final
 PIXEL_ATTRIBUTES: Final[int] = 3
 BLUR_RADIUS: Final[int] = 25
 
+class Dimensions:
+    def __init__(self, image_width, image_height, block_width, block_height, grid_width, grid_height):
+        self.image_width = image_width
+        self.image_height = image_height
+        self.block_width = block_width
+        self.block_height = block_height
+        self.grid_width = grid_width
+        self.grid_height = grid_height
+
+def get_gaussian_kernel():
+    kernel_diameter = BLUR_RADIUS * 2 + 1
+    two_sigma_squared = 2 * pow(BLUR_RADIUS / 2, 2)
+    gaussian_kernel = numpy.empty([kernel_diameter, kernel_diameter], dtype=numpy.float32)
+    for i in range(-BLUR_RADIUS, BLUR_RADIUS + 1):
+        for j in range(-BLUR_RADIUS, BLUR_RADIUS + 1):
+            exponent_numerator = i * i + j * j
+            gaussian_kernel[i + BLUR_RADIUS][j + BLUR_RADIUS] = math.exp(-exponent_numerator / two_sigma_squared) / (two_sigma_squared * math.pi)
+    gaussian_kernel /= gaussian_kernel.sum()
+    gaussian_kernel.flatten().astype(numpy.float32)
+    return gaussian_kernel
+
+def box_blur(dimensions, original_img):
+    # Pad and flatten the image into a 1D array
+    padded_img = numpy.pad(original_img, [(BLUR_RADIUS, BLUR_RADIUS), (BLUR_RADIUS, BLUR_RADIUS), (0, 0)], mode='edge')
+    flattened_img = padded_img.flatten().astype(numpy.uint8)
+    flattened_img_gpu = cuda.mem_alloc(flattened_img.nbytes)
+    # Create an empty result array
+    result = numpy.empty(original_img.size, dtype=numpy.uint8)
+    result_gpu = cuda.mem_alloc(result.nbytes)
+    # Copy input and output arrays to the device
+    cuda.memcpy_htod(flattened_img_gpu, flattened_img)
+    cuda.memcpy_htod(result_gpu, result)
+    # Call the method on the device to blur the image
+    mod = cuda.module_from_file('blur_image_kernel.cubin')
+    box_blur = mod.get_function('BoxBlur')
+    box_blur(flattened_img_gpu,
+                result_gpu,
+                numpy.int32(dimensions.image_width),
+                numpy.int32(dimensions.image_height),
+                block=(dimensions.block_width, dimensions.block_height, 1),
+                grid=(dimensions.grid_width, dimensions.grid_height, PIXEL_ATTRIBUTES))
+    cuda.memcpy_dtoh(result, result_gpu)
+    return result
+
+def gaussian_blur(dimensions, original_img):
+    # Pad and flatten the image into a 1D array
+    padded_img = numpy.pad(original_img, [(BLUR_RADIUS, BLUR_RADIUS), (BLUR_RADIUS, BLUR_RADIUS), (0, 0)], mode='edge')
+    flattened_img = padded_img.flatten().astype(numpy.uint8)
+    flattened_img_gpu = cuda.mem_alloc(flattened_img.nbytes)
+    # Create an empty result array
+    blurred_image = numpy.empty(original_img.size, dtype=numpy.uint8)
+    blurred_image_gpu = cuda.mem_alloc(blurred_image.nbytes)
+    # Copy input and output arrays to the device
+    cuda.memcpy_htod(flattened_img_gpu, flattened_img)
+    cuda.memcpy_htod(blurred_image_gpu, blurred_image)
+    # Generate the gaussian kernel
+    gaussian_kernel = get_gaussian_kernel()
+    gaussian_kernel_gpu = cuda.mem_alloc(gaussian_kernel.nbytes)
+    cuda.memcpy_htod(gaussian_kernel_gpu, gaussian_kernel)
+    # Call the method on the device to blur the image
+    mod = cuda.module_from_file('blur_image_kernel.cubin')
+    gaussian_blur = mod.get_function('GaussianBlur')
+    gaussian_blur(flattened_img_gpu,
+                blurred_image_gpu,
+                gaussian_kernel_gpu,
+                numpy.int32(dimensions.image_width),
+                numpy.int32(dimensions.image_height),
+                block=(dimensions.block_width, dimensions.block_height, 1),
+                grid=(dimensions.grid_width, dimensions.grid_height, PIXEL_ATTRIBUTES))
+    cuda.memcpy_dtoh(blurred_image, blurred_image_gpu)
+    return blurred_image
+
 path = sys.argv[1]
 # Open the image
 original_img = imageio.imread(path, pilmode='RGB')
@@ -22,50 +94,12 @@ image_height = len(original_img)
 block_width, block_height = block_dims.get_optimal_block_dimensions_parallel(image_width, image_height)
 grid_width = math.ceil(image_width / block_width)
 grid_height = math.ceil(image_height / block_height)
+dimensions = Dimensions(image_width, image_height, block_width, block_height, grid_width, grid_height)
 
-# Pad and flatten the image into a 1D array
-padded_img = numpy.pad(original_img, [(BLUR_RADIUS, BLUR_RADIUS), (BLUR_RADIUS, BLUR_RADIUS), (0, 0)], mode='edge')
-flattened_img = padded_img.flatten().astype(numpy.uint8)
-flattened_img_gpu = cuda.mem_alloc(flattened_img.nbytes)
-# Create an empty result array
-result = numpy.empty(original_img.size, dtype=numpy.uint8)
-result_gpu = cuda.mem_alloc(result.nbytes)
-# Copy input and output arrays to the device
-cuda.memcpy_htod(flattened_img_gpu, flattened_img)
-cuda.memcpy_htod(result_gpu, result)
-# Call the method on the device to blur the image
-mod = cuda.module_from_file('blur_image_kernel.cubin')
-"""
-box_blur = mod.get_function('BoxBlur')
-box_blur(flattened_img_gpu,
-            result_gpu,
-            numpy.int32(image_width),
-            numpy.int32(image_height),
-            block=(block_width, block_height, 1),
-            grid=(grid_width, grid_height, PIXEL_ATTRIBUTES))
-"""
-kernel_diameter = BLUR_RADIUS * 2 + 1
-sigma_squared = pow(BLUR_RADIUS / 2, 2)
-gaussian_kernel = numpy.empty([kernel_diameter, kernel_diameter], dtype=numpy.float32)
-for i in range(-BLUR_RADIUS, BLUR_RADIUS + 1):
-    for j in range(-BLUR_RADIUS, BLUR_RADIUS + 1):
-        exponent_numerator = i * i + j * j
-        two_sigma_squared = 2 * sigma_squared
-        gaussian_kernel[i + BLUR_RADIUS][j + BLUR_RADIUS] = math.exp(-exponent_numerator / two_sigma_squared) / (two_sigma_squared * math.pi)
-gaussian_kernel /= gaussian_kernel.sum()
-gaussian_kernel.flatten().astype(numpy.float32)
-gaussian_kernel_gpu = cuda.mem_alloc(gaussian_kernel.nbytes)
-# Copy input and output arrays to the device
-cuda.memcpy_htod(gaussian_kernel_gpu, gaussian_kernel)
-gaussian_blur = mod.get_function('GaussianBlur')
-gaussian_blur(flattened_img_gpu,
-            result_gpu,
-            gaussian_kernel_gpu,
-            numpy.int32(image_width),
-            numpy.int32(image_height),
-            block=(block_width, block_height, 1),
-            grid=(grid_width, grid_height, PIXEL_ATTRIBUTES))
-cuda.memcpy_dtoh(result, result_gpu)
+# Calculate and write the blurred image to disk
+blurred_image = box_blur(dimensions, original_img)
+imageio.imwrite(filepath[0] + '_boxblurred' + str(BLUR_RADIUS) + filepath[1], numpy.reshape(blurred_image.astype(numpy.uint8), (image_height, image_width, PIXEL_ATTRIBUTES)))
 
-# Write the blurred image to disk
-imageio.imwrite(filepath[0] + '_blurred' + str(BLUR_RADIUS) + filepath[1], numpy.reshape(result.astype(numpy.uint8), (image_height, image_width, PIXEL_ATTRIBUTES)))
+# Calculate and write the blurred image to disk
+blurred_image = gaussian_blur(dimensions, original_img)
+imageio.imwrite(filepath[0] + '_gaussianblurred' + str(BLUR_RADIUS) + filepath[1], numpy.reshape(blurred_image.astype(numpy.uint8), (image_height, image_width, PIXEL_ATTRIBUTES)))
